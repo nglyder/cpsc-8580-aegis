3846ce42cf8e1422fd82650fa90c173da5588186
Nick Glyder <nickglyder@gmail.com>
2016-11-11 18:42:28 -0500

    Finished implementing SecurityRule DAO

--- src/aegis/security/DatabaseTest.java
+++ src/aegis/security/DatabaseTest.java
@@ -4,10 +4,5 @@ import java.util.List;
 public class DatabaseTest {
 	public static void main(String[] args) {
 		SecurityRuleDAO rules = new SecurityRuleDAO();
-		List<SecurityRule> all = rules.getAllRules();
-		
-		for(SecurityRule r : all) {
-			System.out.println(r);
-		}
 	}
 }
--- src/aegis/security/SecurityRuleDAO.java
+++ src/aegis/security/SecurityRuleDAO.java
@@ -54,26 +54,89 @@ public class SecurityRuleDAO implements ISecurityRuleDAO {
 
 	@Override
 	public SecurityRule getRuleByID(int id) {
-		// TODO Auto-generated method stub
-		return null;
+        Statement stmt;
+		try {
+			stmt = connection.createStatement();
+	        ResultSet rs = stmt.executeQuery( "SELECT * FROM SECURITY_RULES WHERE "+
+	        								  "ID="+id+";" 
+	        								);
+	        if (rs.next()) {
+	        	SecurityRule rule = new SecurityRule();
+	        	rule.setId(rs.getInt("id"));
+	        	rule.setApplication(rs.getString("application"));
+	        	rule.setDirective(rs.getString("directive"));
+	        	rule.setApi(rs.getString("api"));
+	        	rule.setInstructions(rs.getString("instructions"));
+	        	rule.setInput(rs.getString("input"));
+	        	rule.setOutput(rs.getString("output"));
+	        	return rule;
+	        } else {
+	        	return null;
+	        }
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	@Override
 	public boolean updateRule(SecurityRule rule) {
-		// TODO Auto-generated method stub
-		return false;
+        PreparedStatement stmt;
+		try {
+			stmt = connection.prepareStatement( "UPDATE SECURITY_RULES"+
+												" SET application = ?, directive = ?, api = ?, instructions = ?," + 
+												" input = ?, output = ?" +
+												" WHERE ID="+rule.getId()+";" );
+			stmt.setString(1,rule.getApplication());
+			stmt.setString(2,rule.getDirective());
+			stmt.setString(3,rule.getApi());
+			stmt.setString(4,rule.getInstructions());
+			stmt.setString(5,rule.getInput());
+			stmt.setString(6,rule.getOutput());
+		    stmt.executeUpdate();
+		    stmt.close();
+	        return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
 	public boolean deleteRule(SecurityRule rule) {
-		// TODO Auto-generated method stub
-		return false;
+        Statement stmt;
+		try {
+			stmt = connection.createStatement();
+	        stmt.executeUpdate( "DELETE FROM SECURITY_RULES WHERE "+
+	        								  "ID="+rule.getId()+";" 
+	        								);
+	        return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
 	public boolean createRule(SecurityRule rule) {
-		// TODO Auto-generated method stub
-		return false;
+        PreparedStatement stmt;
+		try {
+			stmt = connection.prepareStatement( "INSERT INTO SECURITY_RULES"+
+												" (application, directive, api, instructions, input, output)" +
+												" values (?,?,?,?,?,?);" );
+			stmt.setString(1,rule.getApplication());
+			stmt.setString(2,rule.getDirective());
+			stmt.setString(3,rule.getApi());
+			stmt.setString(4,rule.getInstructions());
+			stmt.setString(5,rule.getInput());
+			stmt.setString(6,rule.getOutput());
+		    stmt.executeUpdate();
+		    stmt.close();
+	        return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 }
